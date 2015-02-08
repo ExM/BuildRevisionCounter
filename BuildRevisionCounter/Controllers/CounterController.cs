563b675222d43c01e2bdf7e556bf4c84358dcc16
ExM <explorermachine@gmail.com>
2015-02-09 00:14:05 +0300

    минимальный Web API проект

---
+++ BuildRevisionCounter/Controllers/CounterController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+namespace BuildRevisionCounter.Controllers
+{
+	[RoutePrefix("api/counter")]
+	public class CounterController : ApiController
+	{
+		[HttpGet]
+		[Route("{revisionName}")]
+		public async Task<int> Current([FromUri] string revisionName)
+		{
+			//TODO: возвращает текущее значение счетчика "revisionName", если счетчик отсутствует, то возвращать код 404
+			return await Task.FromResult<int>(123);
+		}
+	}
+}
---
+++ BuildRevisionCounter/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Owin;
+using Microsoft.Owin.Extensions;
+using Owin;
+using BuildRevisionCounter;
+using System.Web.Http;
+using System.Net.Http.Formatting;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace BuildRevisionCounter
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			var config = new HttpConfiguration();
+
+			config.Formatters.Clear();
+			config.Formatters.Add(new JsonMediaTypeFormatter()); 
+
+			config.MapHttpAttributeRoutes();
+			config.EnsureInitialized();
+
+			app.UseWebApi(config);
+		}
+	}
+}
