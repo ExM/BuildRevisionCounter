e09c4239ff6d3c153fceb2419e48782dd73607b5
ExM <explorermachine@gmail.com>
2015-02-10 00:35:11 +0300

    минимально работающее решение

--- BuildRevisionCounter/Controllers/CounterController.cs
+++ BuildRevisionCounter/Controllers/CounterController.cs
@@ -1,16 +1,54 @@
-﻿using System.Threading.Tasks;
+﻿using BuildRevisionCounter.Model;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
 namespace BuildRevisionCounter.Controllers
 {
 	[RoutePrefix("api/counter")]
 	public class CounterController : ApiController
 	{
+		private static MongoDBStorage _storage;
+
+		static CounterController()
+		{
+			_storage = new MongoDBStorage();
+		}
+
 		[HttpGet]
 		[Route("{revisionName}")]
-		public async Task<int> Current([FromUri] string revisionName)
+		public long Current([FromUri] string revisionName)
 		{
-			//TODO: возвращает текущее значение счетчика "revisionName", если счетчик отсутствует, то возвращать код 404
-			return await Task.FromResult<int>(123);
+			var q = Query<RevisionModel>.Where(_ => _.Id == revisionName);
+			var revision = _storage.Revisions.FindOne(q);
+
+			if (revision == null)
+				throw new HttpResponseException(HttpStatusCode.NotFound);
+
+			return revision.NextNumber - 1;
+		}
+
+		[HttpPost]
+		[Route("{revisionName:regex([A-Za-z0-9._%+-]*)}")]
+		public long Bumping([FromUri] string revisionName)
+		{
+			var result = _storage.Revisions.FindAndModify(new FindAndModifyArgs()
+			{
+				Query = Query<RevisionModel>.Where(_ => _.Id == revisionName),
+				Upsert = true,
+				Update = Update<RevisionModel>
+					.SetOnInsert(_ => _.Created, DateTime.UtcNow)
+					.Inc(_ => _.NextNumber, 1)
+					.Set(_ => _.Updated, DateTime.UtcNow),
+				VersionReturned = FindAndModifyDocumentVersion.Modified
+			});
+
+			var revision = result.GetModifiedDocumentAs<RevisionModel>();
+
+			return revision.NextNumber - 1;
 		}
 	}
 }
---
+++ BuildRevisionCounter/MongoDBStorage.cs
@@ -0,0 +1,23 @@
+﻿using BuildRevisionCounter.Model;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace BuildRevisionCounter
+{
+	public class MongoDBStorage
+	{
+		public readonly MongoCollection<RevisionModel> Revisions;
+
+		public MongoDBStorage()
+		{
+			string connectionString = ConfigurationManager.ConnectionStrings["MongoDBStorage"].ConnectionString;
+			var database = MongoDatabase.Create(connectionString);
+
+			Revisions = database.GetCollection<RevisionModel>("revisions");
+		}
+	}
+}
