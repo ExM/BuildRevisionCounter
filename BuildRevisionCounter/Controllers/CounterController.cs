c6872fbd8e04927b351e3b833071f3b3f658e83b
Дмитрий Сафонов <dmitriy@safonov.name>
2015-04-15 19:18:16 +0300

    MongoDBStorage теперь можно передавать как параметр в конструктор контроллера. Подготовка к внедрению DI.

--- BuildRevisionCounter/Controllers/CounterController.cs
+++ BuildRevisionCounter/Controllers/CounterController.cs
@@ -1,59 +1,58 @@
-﻿using BuildRevisionCounter.Model;
+﻿using System;
+using System.Net;
+using System.Web.Http;
+using BuildRevisionCounter.Model;
+using BuildRevisionCounter.Security;
 using MongoDB.Driver;
 using MongoDB.Driver.Builders;
-using System;
-using System.Net;
-using System.Threading.Tasks;
-using System.Web;
-using System.Web.Http;
-using BuildRevisionCounter.Security;
 
 namespace BuildRevisionCounter.Controllers
 {
-	[RoutePrefix("api/counter")]
-	[BasicAuthentication()]
-	public class CounterController : ApiController
-	{
-		private static MongoDBStorage _storage;
+    [RoutePrefix("api/counter")]
+    [BasicAuthentication]
+    public class CounterController : ApiController
+    {
+        private readonly MongoDBStorage _storage;
 
-		static CounterController()
-		{
-			_storage = new MongoDBStorage();
-		}
+        public CounterController(MongoDBStorage mongoDbStorage = null)
+        {
+            _storage = mongoDbStorage ?? new MongoDBStorage();
+        }
 
-		[HttpGet]
-		[Route("{revisionName}")]
-		[Authorize(Roles = "admin, editor, anonymous")]
-		public long Current([FromUri] string revisionName)
-		{
-			var q = Query<RevisionModel>.Where(_ => _.Id == revisionName);
-			var revision = _storage.Revisions.FindOne(q);
+        [HttpGet]
+        [Route("{revisionName}")]
+        [Authorize(Roles = "admin, editor, anonymous")]
+        public long Current([FromUri] string revisionName)
+        {
+            var q = Query<RevisionModel>.Where(_ => _.Id == revisionName);
+            var revision = _storage.Revisions.FindOne(q);
 
-			if (revision == null)
-				throw new HttpResponseException(HttpStatusCode.NotFound);
+            if (revision == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
 
-			return revision.NextNumber;
-		}
+            return revision.NextNumber;
+        }
 
-		[HttpPost]
-		[Route("{revisionName}")]
-		[Authorize(Roles = "buildserver")]
-		public long Bumping([FromUri] string revisionName)
-		{
-			var result = _storage.Revisions.FindAndModify(new FindAndModifyArgs()
-			{
-				Query = Query<RevisionModel>.Where(_ => _.Id == revisionName),
-				Upsert = true,
-				Update = Update<RevisionModel>
-					.SetOnInsert(_ => _.Created, DateTime.UtcNow)
-					.Inc(_ => _.NextNumber, 1)
-					.Set(_ => _.Updated, DateTime.UtcNow),
-				VersionReturned = FindAndModifyDocumentVersion.Modified
-			});
+        [HttpPost]
+        [Route("{revisionName}")]
+        [Authorize(Roles = "buildserver")]
+        public long Bumping([FromUri] string revisionName)
+        {
+            var result = _storage.Revisions.FindAndModify(
+                new FindAndModifyArgs
+                {
+                    Query = Query<RevisionModel>.Where(_ => _.Id == revisionName),
+                    Upsert = true,
+                    Update = Update<RevisionModel>
+                        .SetOnInsert(_ => _.Created, DateTime.UtcNow)
+                        .Inc(_ => _.NextNumber, 1)
+                        .Set(_ => _.Updated, DateTime.UtcNow),
+                    VersionReturned = FindAndModifyDocumentVersion.Modified
+                });
 
-			var revision = result.GetModifiedDocumentAs<RevisionModel>();
+            var revision = result.GetModifiedDocumentAs<RevisionModel>();
 
-			return revision.NextNumber;
-		}
-	}
+            return revision.NextNumber;
+        }
+    }
 }
