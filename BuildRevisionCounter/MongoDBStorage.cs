727fa10a7a7f035883dbe2ed83127225dce9f960
Дмитрий Сафонов <dmitriy@safonov.name>
2015-04-17 13:42:47 +0300

    Первая версия юнит тестов контроллера.

--- BuildRevisionCounter/MongoDBStorage.cs
+++ BuildRevisionCounter/MongoDBStorage.cs
@@ -20,12 +20,26 @@ namespace BuildRevisionCounter
 			Users = database.GetCollection<UserModel>("users");
 		}
 
+		public async Task SetUp()
+		{
+			await Users.Indexes.CreateOneAsync(
+				Builders<UserModel>.IndexKeys.Ascending(u => u.Name),
+				new CreateIndexOptions {Unique = true});
+
+			await EnsureAdminUser();
+		}
+
 		public async Task EnsureAdminUser()
 		{
 			if (await Users.CountAsync(_ => true) == 0)
 			{
 				await CreateUser(AdminName, AdminPassword, AdminRoles);
 			}
+		}
+
+		public async Task<UserModel> FindUser(string name)
+		{
+			return await Users.Find(u => u.Name == name).SingleOrDefaultAsync();
 		}
 
 		public Task CreateUser(string name, string password, string[] roles)
