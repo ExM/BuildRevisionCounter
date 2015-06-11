e04a586f5e61697e76cc81fe283c7c15eed3247c
Timofey Ermak <ermak_timofey@hotmail.com.com>
2015-06-11 13:37:40 +0500

    Добавлен уровень данных .Model

---
+++ BuildRevisionCounter.Model/BuildRevisionStorage/RevisionModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BuildRevisionCounter.Model.BuildRevisionStorage
+{
+	public class RevisionModel
+	{
+		[BsonId]
+		public string Id;
+
+		[BsonElement("created"), BsonDateTimeOptions(Kind=DateTimeKind.Utc)]
+		public DateTime Created;
+
+		[BsonElement("updated"), BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+		public DateTime Updated;
+
+		[BsonElement("nextNumber")]
+		public long NextNumber;
+
+	}
+}
